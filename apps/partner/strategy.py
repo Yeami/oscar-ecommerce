fbfcaf7f4a09328830d80d1ddc56ccea584e9981
Yeami <ivanov.nikita@yeami.group>
2020-06-21 01:07:29 +0300

    Improve the functionality to display the different product`s price depending on user partner group

--- apps/partner/strategy.py
+++ apps/partner/strategy.py
@@ -1,3 +1,4 @@
+from oscar.apps.partner.models import Partner
 from oscar.apps.partner.strategy import Default as CoreDefault
 
 
@@ -11,16 +12,30 @@ class Default(CoreDefault):
     Partner strategy, that returns prices from stockrecord
     depending on user partner group
     """
+    product = None
+    partner = None
 
     def select_stockrecord(self, product):
+        self.product = product
         try:
-            from oscar.apps.partner.models import Partner
-
-            if self.user:
-                partner = Partner.objects.filter(users=self.user.id)[0]
-            else:
-                partner = Partner.objects.filter(code='default')[0]
-
-            return product.stockrecords.filter(partner=partner)[0]
+            return self.get_product_for_auth_user() if self.user else self.get_default_product()
         except IndexError:
             return None
+
+    def get_product_for_auth_user(self):
+        self.partner = Partner.objects.filter(users=self.user.id)
+
+        return self.get_product_for_partner() if len(self.partner) else self.get_default_product()
+
+    def get_product_for_partner(self):
+        product = self.get_partner_product(self.partner)
+
+        return product[0] if len(product) else self.get_default_product()
+
+    def get_partner_product(self, partner):
+        return self.product.stockrecords.filter(partner=partner[0])
+
+    def get_default_product(self):
+        partner = Partner.objects.filter(code='default')[0]
+
+        return self.product.stockrecords.filter(partner=partner)[0]
